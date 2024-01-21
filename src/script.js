a4dc943611454fefea1a521a6a54860cf478befc
John Samuel <johnayobami77@proton.me>
2024-01-21 20:26:48 +0100

    Updated script and index

--- src/script.js
+++ src/script.js
@@ -1,9 +1,10 @@
 function updateClock() {
     const now = new Date();
-    const timeOptions = { hour: '2-digit', minute: '2-digit', second: '2-digit' };
+    const timeOptions = { hour: '2-digit', minute: '2-digit'};
     const timeString = now.toLocaleTimeString('en-US', timeOptions);
     document.getElementById("time").textContent = timeString;
 }
 
 setInterval(updateClock, 1000);
 updateClock();
+
