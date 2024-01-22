06ca4bcf0082afd0706da3457b079002108cba70
John Samuel <johnayobami77@proton.me>
2024-01-23 00:12:35 +0100

    updates

--- src/script.js
+++ src/script.js
@@ -9,6 +9,7 @@ setInterval(updateClock, 1000);
 updateClock();
 
 
+
 //Password
 document.addEventListener('DOMContentLoaded', function () {
     const passwordInput = document.getElementById('password');
@@ -23,3 +24,39 @@ document.addEventListener('DOMContentLoaded', function () {
     // Event listener for clicking on the showPassword element
     showPasswordIcon.addEventListener('click', togglePasswordVisibility);
 });
+
+
+//Password\\
+function validatePassword() {
+    // Get the entered password
+    var enteredPassword = document.getElementById("password").value;
+
+    // Expected password
+    var expectedPassword = "123456";
+
+    // Check if the entered password matches the expected password
+    if (enteredPassword === expectedPassword) {
+        console.log("Login successful!");
+        return true; // Allow form submission
+    } else {
+        console.log("Incorrect password. Please try again.");
+        return false; // Prevent form submission
+    }
+}
+
+function togglePasswordVisibility() {
+    var passwordInput = document.getElementById("password");
+    var showPasswordIcon = document.getElementById("showPassword");
+    var hidePasswordIcon = document.getElementById("hidePassword");
+
+    // Toggle password visibility
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        showPasswordIcon.style.display = "none";
+        hidePasswordIcon.style.display = "block";
+    } else {
+        passwordInput.type = "password";
+        showPasswordIcon.style.display = "block";
+        hidePasswordIcon.style.display = "none";
+    }
+}
