216e827bf682c944d82f504d4093fc6313245565
John Samuel <johnayobami77@proton.me>
2024-01-23 17:45:54 +0100

    updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,html,js}"],
   theme: {
     fontFamily: {
       'serif':['Open Sans'],
